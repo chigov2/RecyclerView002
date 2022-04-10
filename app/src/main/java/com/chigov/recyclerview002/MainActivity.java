af451fdf1b96817849b58766a44d6628b8e5d5c1
Max2 <chigov@gmail.com>
2022-04-10 21:58:09 +0300

    12min

--- app/src/main/java/com/chigov/recyclerview002/MainActivity.java
+++ app/src/main/java/com/chigov/recyclerview002/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.chigov.recyclerview002.adapters.EmployeeAdapter;
+import com.chigov.recyclerview002.api.ApiFactory;
+import com.chigov.recyclerview002.api.ApiService;
 import com.chigov.recyclerview002.pojo.Employee;
 
 import java.util.ArrayList;
@@ -24,25 +26,29 @@ public class MainActivity extends AppCompatActivity {
         adapter = new EmployeeAdapter();
         recyclerViewEmployees.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewEmployees.setAdapter(adapter);
+        ApiFactory apiFactory = ApiFactory.getInstance();
+        ApiService apiService = apiFactory.getApiService();
 
-        List<Employee> employees = new ArrayList<>();
-        Employee employee1 = new Employee();
-        Employee employee2 = new Employee();
-        Employee employee3 = new Employee();
-        Employee employee4 = new Employee();
-        employee1.setfName("Max");
-        employee2.setfName("Ivan");
-        employee1.setlName("Ivanov");
-        employee2.setlName("Petrov");
-        employee3.setfName("Max");
-        employee4.setfName("Ivan");
-        employee3.setlName("Ivanov");
-        employee4.setlName("Petrov");
 
-        employees.add(employee1);
-        employees.add(employee2);
-        employees.add(employee3);
-        employees.add(employee4);
-        adapter.setEmployees(employees);
     }
 }
+
+//    List<Employee> employees = new ArrayList<>();
+//    Employee employee1 = new Employee();
+//    Employee employee2 = new Employee();
+//    Employee employee3 = new Employee();
+//    Employee employee4 = new Employee();
+//        employee1.setfName("Max");
+//                employee2.setfName("Ivan");
+//                employee1.setlName("Ivanov");
+//                employee2.setlName("Petrov");
+//                employee3.setfName("Max");
+//                employee4.setfName("Ivan");
+//                employee3.setlName("Ivanov");
+//                employee4.setlName("Petrov");
+//
+//                employees.add(employee1);
+//                employees.add(employee2);
+//                employees.add(employee3);
+//                employees.add(employee4);
+//                adapter.setEmployees(employees);
---
+++ app/src/main/java/com/chigov/recyclerview002/api/ApiService.java
@@ -0,0 +1,11 @@
+package com.chigov.recyclerview002.api;
+
+import com.chigov.recyclerview002.pojo.EmployeeResponse;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    @GET("testTask.json")
+    Observable<EmployeeResponse> getEmployees();
+}
