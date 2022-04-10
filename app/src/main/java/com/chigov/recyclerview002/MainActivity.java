901bf625f9718dfb8e98dda8c8f399f5c03a73ce
Max2 <chigov@gmail.com>
2022-04-10 11:54:56 +0300

    Initial commit

---
+++ app/src/main/java/com/chigov/recyclerview002/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chigov.recyclerview002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
