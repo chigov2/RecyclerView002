f0c7d783c1600d7c3437700486d230b45057ef55
Max2 <chigov@gmail.com>
2022-04-10 13:55:22 +0300

    Local rec_view works

--- app/src/main/java/com/chigov/recyclerview002/MainActivity.java
+++ app/src/main/java/com/chigov/recyclerview002/MainActivity.java
@@ -1,14 +1,39 @@
 package com.chigov.recyclerview002;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.chigov.recyclerview002.adapters.EmployeeAdapter;
+import com.chigov.recyclerview002.pojo.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewEmployees;
+    private EmployeeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewEmployees = findViewById(R.id.recyclerViewEmployee);
+        adapter = new EmployeeAdapter();
+        recyclerViewEmployees.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewEmployees.setAdapter(adapter);
+
+        List<Employee> employees = new ArrayList<>();
+        Employee employee1 = new Employee();
+        Employee employee2 = new Employee();
+        employee1.setfName("Max");
+        employee2.setfName("Ivan");
+        employee1.setlName("Ivanov");
+        employee2.setlName("Petrov");
+        employees.add(employee1);
+        employees.add(employee2);
+        adapter.setEmployees(employees);
     }
 }
